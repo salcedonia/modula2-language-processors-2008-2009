2abc137dd816dfdb2fbae2f2408149435adf9548
mardani29 <mardani29@d3fd9f16-0741-11de-8884-7b8501395459>
2009-03-10 13:46:48 +0000

    Prueba

--- trunk/src/Parser/SlkAction.java
+++ trunk/src/Parser/SlkAction.java
@@ -14,31 +14,67 @@ public class SlkAction {
 	public void execute ( int number ){
 		switch ( number ) {
             case 1:  unidadDeCompilacion1();  break;
-            case 2:  unidadDeCompilacion2();  break;
-            case 3:  unidadDeCompilacion3();  break;
-            // Grupo 3 - Daniel Martín
-            case 4:  SentenciaWHILE();  break;
-            case 5:  SentenciaREPEAT();  break;
-            case 6:  SentenciaLOOP();  break;
-            case 7:  SentenciaFOR();  break;
-            case 8:  SentenciaWITH();  break;
-            case 9:  SentenciaRETURN();  break;
-            case 10:  ListaDeValores();  break;
-            case 11:  vacio();  break;
-            case 12:  RestoValores();  break;
-            // Siguiente persona
-            case 13:  numEntero();  break;
-            case 14:  numReal();  break;
+    case 2:  unidadDeCompilacion2();  break;
+    case 3:  unidadDeCompilacion3();  break;
+    case 4:  SentenciaAsignacion();  break;
+    case 5:  RestoSentenciaAsignacion();  break;
+    case 6:  RestoSentenciaRestoAsignacion();  break;
+    case 7:  Expresion();  break;
+    case 8:  SentenciaWHILE();  break;
+    case 9:  SentenciaREPEAT();  break;
+    case 10:  SentenciaLOOP();  break;
+    case 11:  SentenciaFOR();  break;
+    case 12:  SentenciaWITH();  break;
+    case 13:  SentenciaRETURN();  break;
+    case 14:  ListaDeValores();  break;
+    case 15:  vacio();  break;
+    case 16:  RestoValores();  break;
+    case 17:  RestoExpresion();  break;
+    case 18:  ExpresionSimple();  break;
+    case 19:  RestoExpresionSimple();  break;
+    case 20:  termino();  break;
+    case 21:  factor();  break;
+    case 22:  numero();  break;
+    case 23:  numEntero();  break;
+    case 24:  numReal();  break;
         }
 		
 	}
+
+    private void Expresion() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void ExpresionSimple() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
     private void ListaDeValores() {
         System.out.println("---ListaDeValores---");
     }
 
+    private void RestoExpresion() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void RestoExpresionSimple() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void RestoSentenciaAsignacion() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void RestoSentenciaRestoAsignacion() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
     private void RestoValores() {
         System.out.println("---RestoValores---");
+    }
+
+    private void SentenciaAsignacion() {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     private void SentenciaFOR() {
@@ -66,6 +102,10 @@ public class SlkAction {
         System.out.println("---SentenciaWITH---");
     }
 
+    private void factor() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
     private void numEntero() {
         System.out.println("---numEntero---");
         Nodo entero = new Nodo();
@@ -80,6 +120,15 @@ public class SlkAction {
         ArrayList array = entero.getArray();
         array.add("Real");
         entero.setTipo(array);
+    }
+
+    private void numero() {
+        System.out.println("---numero---");
+        Nodo numero = new Nodo();
+    }
+
+    private void termino() {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     private void unidadDeCompilacion1() {
